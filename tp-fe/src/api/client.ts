d3c613366b8e29d7498cfc55362dec53b21a2ec9
eai04191 <eai@mizle.net>
2024-12-11 03:02:20 +0900

    env

--- tp-fe/src/api/client.ts
+++ tp-fe/src/api/client.ts
@@ -6,7 +6,7 @@ import {
     User,
 } from "../types";
 
-const API_BASE_URL = "http://localhost:3001/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
 
 class ApiClient {
     private token: string;
